b3b05a38ebccefe3a424cc193c9ae4ebb792e67c
saarhin <sarah77.amini@gmail.com>
2025-06-21 17:55:33 -0600

    cc run

--- gym_cooking/CC/run_mini.sh
+++ gym_cooking/CC/run_mini.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=1             # Number of CPUs per task
 #SBATCH --mem=4G                     # Memory per task
-#SBATCH --array=0-19                  # Array index range (adjust based on parameter file size)
+#SBATCH --array=0-1                  # Array index range (adjust based on parameter file size)
 #SBATCH --mail-user=samini1@ualberta.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --account=def-mtaylor3
