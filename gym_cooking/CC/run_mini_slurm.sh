2edffdab9367faf6ff6f08839d60244ebacba44d
saarhin <sarah77.amini@gmail.com>
2025-06-30 14:43:46 -0600

    mem-per-cpu CC

--- gym_cooking/CC/run_mini_slurm.sh
+++ gym_cooking/CC/run_mini_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=1             # Number of CPUs per task
 #SBATCH --ntasks=8
-#SBATCH --mem=4G                     # Memory per task
+#SBATCH --mem-per-cpu=4G
 #SBATCH --array=0-1                  # Array index range (adjust based on parameter file size)
 #SBATCH --mail-user=samini1@ualberta.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
