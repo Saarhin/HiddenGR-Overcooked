154663d970d0afb28357bd5ed535089d0d66329e
saarhin <sarah77.amini@gmail.com>
2025-06-30 12:28:28 -0600

    test minibatch slurm on cc

---
+++ gym_cooking/CC/run_mini_slurm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=parallel_jobs       # Job name
+#SBATCH --error=/home/saarhin/scratch/HiddenGR-Overcooked/gym_cooking/error_log/error_%A_%a.log       # Error log file  for each task
+#SBATCH --output=/home/saarhin/scratch/HiddenGR-Overcooked/gym_cooking/out_log/output_%A_%a.out 
+#SBATCH --time=2:00:00
+#SBATCH --cpus-per-task=1             # Number of CPUs per task
+#SBATCH --mem=4G                     # Memory per task
+#SBATCH --array=0-1                  # Array index range (adjust based on parameter file size)
+#SBATCH --mail-user=samini1@ualberta.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --account=def-mtaylor3
+
+SRC=/home/saarhin/scratch/HiddenGR-Overcooked
+
+DEST=$SLURM_TMPDIR/
+
+rsync -avh "$SRC/" "$DEST/"
+
+cd $SLURM_TMPDIR/HiddenGR-Overcooked
+
+
+module load python/3.10
+
+source $SLURM_TMPDIR/HiddenGR-Overcooked/marl/bin/activate
+
+export PYTHONPATH="${PYTHONPATH}:$SLURM_TMPDIR/HiddenGR-Overcooked"
+
+cd gym_cooking
+
+PARAMS=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" ./CC/parameters_mini.txt)
+
+python main.py $PARAMS
+
+SRC_BASE=$SLURM_TMPDIR/HiddenGR-Overcooked/gym_cooking
+
+DEST=/home/saarhin/scratch/HiddenGR-Overcooked/gym_cooking/results
+
+rsync -avh "${SRC_BASE}"/policies_*  "$DEST"/
+
--- gym_cooking/test/reward_plot.py
+++ gym_cooking/test/reward_plot.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import outset as otst
 
-input_file = "./policies_Summary_seed3/rewards.csv"
+input_file = "./policies_Summary_seed10_fetcherDQN_simpleChef/rewards_207000.csv"
 
 file = pd.read_csv(input_file)
 file_array = np.array(file)
 
 # adapted from https://matplotlib.org/stable/gallery/
-x,y = file_array[:,0], file_array[:,1]
+x,y = file_array[:207000,0], file_array[:207000,1]
 
 # print(np.max(file_array[:,1]))
 # plt.plot(file_array[:,0], file_array[:,1])
@@ -18,20 +18,34 @@ x,y = file_array[:,0], file_array[:,1]
 
 
 # 3 axes grid: source plot and two zoom frames
-grid = otst.OutsetGrid([(3000, -25, 3500, -15)])  # frame coords
-grid.broadcast(
-    plt.plot,  # run plotter over all axes
-    x,
-    y,  # line coords
-    c="mediumblue",
-    zorder=-1,
-)  # kwargs forwarded to plt.plot
+# grid = otst.OutsetGrid([(3000, -25, 3500, -15)])  # frame coords
+# grid.broadcast(
+#     plt.plot,  # run plotter over all axes
+#     x,
+#     y,  # line coords
+#     c="mediumblue",
+#     zorder=-1,
+# )  # kwargs forwarded to plt.plot
 
-plt.xlabel("Training Episodes")
-plt.ylabel("Return")
-
-
+# plt.xlabel("Training Episodes")
+# plt.ylabel("Return")
 
 
-grid.marqueeplot()  # set axlims and render marquee annotations
+
+
+
+
+# grid.marqueeplot()  # set axlims and render marquee annotations
+# plt.show()
+
+def moving_average(x, window_size):
+    return np.convolve(x, np.ones(window_size)/window_size, mode='valid')
+
+smoothed = moving_average(y, window_size=50)
+
+plt.plot(y, alpha=0.3, label="Raw reward")
+plt.plot(range(len(smoothed)), smoothed, label="Moving Avg (50)")
+plt.xlabel("Episode")
+plt.ylabel("Reward")
+plt.legend()
 plt.show()
